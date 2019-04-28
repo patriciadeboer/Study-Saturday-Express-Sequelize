c8f3cb7ffc06eb7e8a4d19713bdeab0c9f0275ef
Patricia de Boer <pdeboer05@gmail.com>
2019-04-28 14:55:11 -0500

    finialized lab

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 
 app.use('/students', students);
 app.use('/tests', tests);
--- routes/tests.js
+++ routes/tests.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
-
+const Test = require('../db/models/test');
+const Student = require('../db/models/student');
 module.exports = router;
 
 router.get('/', async (req, res, next) => {
   try {
-
+    const allTests= await Test.findAll();
+    res.send(allTests)
   } catch (err) {
     next(err);
   }
@@ -12,7 +14,15 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   try {
-
+    //when using findAll need brackets
+    const [getTest]= await Test.findAll({
+      where:{
+        id:req.params.id,
+      }
+    })
+    res.send(getTest)
+    // const getTest= await Test.findById(req.params.id)
+    // res.send(getTest)
   } catch (err) {
     next(err);
   }
@@ -20,7 +30,17 @@ router.get('/:id', async (req, res, next) => {
 
 router.post('/student/:studentId', async (req, res, next) => {
   try {
+    const [getStudent]= await Student.findAll({
+      where:{
+        id:req.params.studentId
+      }
+    })
 
+    const createTest = await Test.create(req.body)
+
+    await createTest.setStudent(getStudent)
+
+    res.status(201).send(createTest)
   } catch (err) {
     next(err);
   }
@@ -28,7 +48,12 @@ router.post('/student/:studentId', async (req, res, next) => {
 
 router.delete('/:id', async (req, res, next) => {
   try {
-
+    const deleteTest = await Test.destroy({
+      where:{
+        id:req.params.id,
+      }
+    })
+    res.status(204).send()
   } catch (err) {
     next(err);
   }
