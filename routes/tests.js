f01bdcac6a14e259a1699e4be6e378b5116068a0
Patricia de Boer <pdeboer05@gmail.com>
2019-04-28 14:07:26 -0500

    initial test route set up

--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,35 @@
 const router = require('express').Router();
 
-
 module.exports = router;
+
+router.get('/', async (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/student/:studentId', async (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err);
+  }
+});
